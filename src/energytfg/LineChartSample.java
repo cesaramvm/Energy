38a828ee4e2d5f2f03ae87358a684e9f4091779a
César Valdés <cesaramvm@gmail.com>
2016-02-14 12:44:44 +0100

    Limpieza

--- src/energytfg/LineChartSample.java
+++ src/energytfg/LineChartSample.java
@@ -10,72 +10,19 @@ package energytfg;
  * @author sobremesa
  */
 import java.util.ArrayList;
-import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
-public class LineChartSample extends Application {
+public class LineChartSample {
 
     private static ArrayList<Double> data;
     private static String mseChartTitle;
-
-    @Override
-    public void start(Stage stage) {
-        stage.setTitle("Line Chart");
-        //defining the axes
-        NumberAxis xAxis = new NumberAxis();
-        NumberAxis yAxis = new NumberAxis();
-        xAxis.setLabel("Epochs");
-        yAxis.setLabel("MSE");
-        //creating the chart
-        LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
-
-        lineChart.setTitle("MSE Chart for " + mseChartTitle);
-        //defining a series
-        XYChart.Series series = new XYChart.Series();
-        series.setName("My portfolio");
-        //populating the series with data
-        for (int i = 0; i < data.size(); i++) {
-            Double d = data.get(i);
-            series.getData().add(new XYChart.Data(i, d));
-        }
-
-        Scene scene = new Scene(lineChart, 1000, 600);
-        lineChart.getData().add(series);
-
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public void execute(ArrayList<Double> incomingData, String chartTitle) {
-        data = incomingData;
-        mseChartTitle = chartTitle;
-        launch();
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 
     private static void initAndShowGUI() {
         // This method is invoked on the EDT thread
@@ -86,32 +33,26 @@ public class LineChartSample extends Application {
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        Platform.runLater(new Runnable() {
+        Platform.runLater(() -> {
+            initFX(fxPanel);
+        });
+        /* same as
+                Platform.runLater(new Runnable() {
             @Override
             public void run() {
                 initFX(fxPanel);
             }
         });
+         */
     }
 
     private static void initFX(JFXPanel fxPanel) {
-        // This method is invoked on the JavaFX thread
         Scene scene = createScene();
         fxPanel.setScene(scene);
         fxPanel.setVisible(true);
     }
 
     private static Scene createScene() {
-//        Group  root  =  new  Group();
-//        Scene  scene  =  new  Scene(root, Color.ALICEBLUE);
-//        Text  text  =  new  Text();
-//        
-//        text.setX(40);
-//        text.setY(100);
-//        text.setFont(new Font(25));
-//        text.setText("Welcome JavaFX!");
-//
-//        root.getChildren().add(text);
 
         NumberAxis xAxis = new NumberAxis();
         NumberAxis yAxis = new NumberAxis();
@@ -131,7 +72,7 @@ public class LineChartSample extends Application {
         }
 
         Scene scene = new Scene(lineChart, 1000, 600);
-        lineChart.getData().add(series);
+        lineChart.getData().addAll(series);
 
         return (scene);
     }
@@ -139,13 +80,18 @@ public class LineChartSample extends Application {
     public static void main(ArrayList<Double> incomingData, String chartTitle) {
         data = incomingData;
         mseChartTitle = chartTitle;
-        
+
+        SwingUtilities.invokeLater(() -> {
+            initAndShowGUI();
+        });
+        /*same as
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
                 initAndShowGUI();
             }
         });
+        */
     }
 
 }
